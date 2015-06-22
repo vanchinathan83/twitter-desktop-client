8de51c7d34e7094910b0e0805958a9e4bc0f58c2
vanchinathanac <ac.vanchinathan@gmail.com>
2015-06-21 23:42:37 -0500

    adding twitter api node module. Getting there for oauth access

--- script/app.js
+++ script/app.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var app = express();
+var twitterAPI = require('node-twitter-api');
 
-
-app.get("/", function(request,response){
-  response.send("Hello World");
+var twitter = new twitterAPI({
+    consumerKey: 'unknown',
+    consumerSecret: 'unknowm',
+    callback: 'oob'
 });
+var params = { nonce : 'DCBgDRabcdefghihdjskjsdfjksdafkjhadfjkhsadkjfh', timestamp: 1434946770};
+
+app.get("/twitter_access", function(request,response){
+  twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+    if (error) {
+        response.send(error);
+    } else {
+        response.redirect(301,"https://twitter.com/oauth/authenticate?oauth_token="+requestToken);
+    }
+  });
+});
+app.get("/", function(request,response){
+  response.send("<a href=\"twitter_access\">Check the Twitter Access</a>");
+});
+
 
 var server = app.listen(2332, function(error){
   if(error){
